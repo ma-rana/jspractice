50994e920cd289277ba30d1a9153dafc8a103b32
basanta-rana <basanta.rana.ma@gmail.comgit config --global user.name ma-rana>
2025-07-17 16:57:43 +1000

    adding Two Numbers - addTwoNumbers.js

---
+++ day1/addTwoNumbers.js
@@ -0,0 +1,13 @@
+function addTwoNUmbers(numA, numB) {
+  return numA + numB;
+}
+const numA = 5;
+const numB = 10;
+console.log(addTwoNUmbers(numA, numB)); // Output: 15
+
+//another example
+const result = addTwoNUmbers(numA, numB);
+console.log(result); // Output: 15
+
+//another example
+console.log(addTwoNUmbers(20, 30)); // Output: 50
